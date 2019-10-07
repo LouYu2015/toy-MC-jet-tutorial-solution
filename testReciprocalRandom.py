665419bd5398f6e69af32beb560d44c830c9edad
Tom Lou <louyu27@cs.washington.edu>
2019-10-07 10:47:00 -0700

    Add label to plot

--- testReciprocalRandom.py
+++ testReciprocalRandom.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     r = [ReciprocalRandom.ReciprocalRandom.generate(1, 5)
-         for _ in range(10000)]
-    plt.hist(r)
+         for _ in range(100000)]
+    plt.title("Reciprocal Function")
+    plt.xlabel("Value")
+    plt.ylabel("Number of samples")
+    plt.hist(r, bins=100)
     plt.show()
